d375534616c1060e71ef3addc5733281a908a3a3
Paulo Toledo <paulojrtoledo@gmail.com>
2025-09-29 21:36:06 -0300

    Adicionando produtos via firestore databse

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,13 +1,20 @@
-import admin from 'firebase-admin';
-import fs from 'fs';
-import dotenv from 'dotenv';
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
 
-dotenv.config(); // garante que process.env está carregado
+dotenv.config();
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(process.env.FIREBASE_SERVICE_ACCOUNT as string, 'utf-8')
-);
+// Pega o caminho da chave do .env
+const serviceAccountPath = path.resolve(__dirname, "../firebase-service/firebase-service-key.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// Lê e transforma em objeto
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export const db = admin.firestore();
